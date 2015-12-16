8f834d75647caf25ca8bb48767ab0e54d137bf06
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-12-16 14:34:38 +0100

    test(resource): refactor to use apiServer

--- test/resource.spec.js
+++ test/resource.spec.js
@@ -2,7 +2,7 @@
 import Resource from '../source/resource';
 import test from 'tape';
 import has from 'lodash/object/has';
-import { server, fixtures } from './helpers';
+import { server, apiServer } from './helpers';
 import got from 'got';
 
 test('Resource fetch', (t) => {
@@ -35,49 +35,31 @@ test('Resource data', (t) => {
 test('Resource links with default `links` attributes', (t) => {
   t.plan(2);
 
-  const request = (url) => {
-    return got(url, { json: true });
-  };
+  apiServer('resource-links.json').then((api) => {
+    Resource
+      .fetch('/person/1', api.options)
+      .then((resource) => {
+        const links = resource.links();
 
-  fixtures('resource-links.json')
-    .then((data) => { return data.routes })
-    .then((routes) => { return server(routes); })
-    .then((api) => {
-      /* override to force json */
-      api.request(request);
-
-      Resource
-        .fetch('/person/1', api.options)
-        .then((resource) => {
-          const links = resource.links();
-
-          t.ok(has(links, 'self'), 'should have the `self` link');
-          t.ok(has(links, 'house'), 'should have the `house` link');
-        });
-    });
+        t.ok(has(links, 'self'), 'should have the `self` link');
+        t.ok(has(links, 'house'), 'should have the `house` link');
+      });
+  });
 });
 
-test('Resource links with default `_links` attributes', (t) => {
+test('Resource links with `_links` attributes', (t) => {
   t.plan(2);
 
-  fixtures('resource-_links.json')
-    .then((data) => { return data.routes; })
-    .then((routes) => { return server(routes); })
-    .then((api) => {
-      /* override to force json */
-      api.request((url) => {
-        return got(url, { json: true });
+  apiServer('resource-_links.json').then((api) => {
+    api.options.linkAttr = '_links';
+
+    Resource
+      .fetch('/person/1', api.options)
+      .then((resource) => {
+        const links = resource.links();
+
+        t.ok(has(links, 'self'), 'should have the `self` link');
+        t.ok(has(links, 'house'), 'should have the `house` link');
       });
-
-      api.options.linkAttr = '_links';
-
-      Resource
-        .fetch('/person/1', api.options)
-        .then((resource) => {
-          const links = resource.links();
-
-          t.ok(has(links, 'self'), 'should have the `self` link');
-          t.ok(has(links, 'house'), 'should have the `house` link');
-        });
-    });
+  });
 });
