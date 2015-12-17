71fd5b6a03a8cc3d7d6345a4373cb26342b1501f
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-12-17 16:31:09 +0100

    Release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+<a name="0.1.0"></a>
+# 0.1.0 (2015-12-17)
+
+
+### Bug Fixes
+
+* **test:** update test plan count ([f9b31a3](https://github.com/stephanebachelier/halapi/commit/f9b31a3))
+* **test:** use absolute url ([8d17d13](https://github.com/stephanebachelier/halapi/commit/8d17d13))
+
+### Features
+
+* **api:** add request helper ([6b78b73](https://github.com/stephanebachelier/halapi/commit/6b78b73))
+* **api:** add resource helper ([983f5df](https://github.com/stephanebachelier/halapi/commit/983f5df))
+* **api:** add resource to wrap resource fetch ([640130c](https://github.com/stephanebachelier/halapi/commit/640130c))
+* **api:** add url() helper ([aec4520](https://github.com/stephanebachelier/halapi/commit/aec4520))
+* **api:** implement constructor ([87e8fca](https://github.com/stephanebachelier/halapi/commit/87e8fca))
+* **resource:** add data() helper ([3675b74](https://github.com/stephanebachelier/halapi/commit/3675b74))
+* **resource:** add get() helper ([20448b4](https://github.com/stephanebachelier/halapi/commit/20448b4))
+* **resource:** add link() method ([b6f9145](https://github.com/stephanebachelier/halapi/commit/b6f9145))
+* **resource:** add links property ([ff5f90c](https://github.com/stephanebachelier/halapi/commit/ff5f90c))
+* **resource:** save the response body to data ([f53fbb5](https://github.com/stephanebachelier/halapi/commit/f53fbb5))
+* **utils:** add url helper ([807dca3](https://github.com/stephanebachelier/halapi/commit/807dca3))
+
+
+
---
+++ dist/halapi.umd.js
@@ -0,0 +1,150 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('lodash')) :
+  typeof define === 'function' && define.amd ? define(['lodash'], factory) :
+  global.halapi = factory(global._);
+}(this, function (_) { 'use strict';
+
+  _ = 'default' in _ ? _['default'] : _;
+
+  var babelHelpers = {};
+
+  babelHelpers.classCallCheck = function (instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  };
+
+  babelHelpers.createClass = (function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  })();
+
+  babelHelpers;
+  var utils = {
+    url: function url(endpoint, path) {
+      var len = endpoint.length;
+
+      return [endpoint.charAt(len - 1) === '/' ? endpoint.substr(0, len - 1) : endpoint, path.charAt(0) === '/' ? path.substring(1) : path].join('/');
+    }
+  };
+
+  var Resource = (function () {
+    babelHelpers.createClass(Resource, null, [{
+      key: 'request',
+      value: function request(url) {
+        throw new Error('Not implemented');
+      }
+    }, {
+      key: 'fetch',
+      value: function fetch(path, options) {
+        var endpoint = options.endpoint;
+
+        var url = utils.url(endpoint, path);
+
+        return Resource.request(url).then(function (response) {
+          var resource = new Resource(path, options);
+
+          resource.save(response.body);
+          return resource;
+        });
+      }
+    }]);
+
+    function Resource(path, options) {
+      babelHelpers.classCallCheck(this, Resource);
+
+      this._path = path;
+      this.options = options;
+      this.linkAttr = options.linkAttr || 'links';
+    }
+
+    babelHelpers.createClass(Resource, [{
+      key: 'save',
+      value: function save(body) {
+        this._data = body;
+      }
+    }, {
+      key: 'data',
+      value: function data() {
+        return _.omit(this._data, this.linkAttr);
+      }
+    }, {
+      key: 'get',
+      value: function get(name) {
+        return (this._data || {})[name];
+      }
+    }, {
+      key: 'links',
+      value: function links() {
+        return this._data[this.options.linkAttr];
+      }
+    }, {
+      key: 'link',
+      value: function link(name) {
+        var _links = this.links();
+
+        if (!_links) {
+          return null;
+        }
+
+        var _link = _links[name];
+
+        if (!_link) {
+          return null;
+        }
+
+        if (_link.href) {
+          return Resource.fetch(_link.href, this.options);
+        }
+
+        return null;
+      }
+    }]);
+    return Resource;
+  })();
+
+  var HalApi = (function () {
+    function HalApi(options) {
+      babelHelpers.classCallCheck(this, HalApi);
+
+      this.options = _.defaults({
+        linkAttr: 'links'
+      }, options || {});
+
+      if (!_.has(this.options, 'endpoint')) {
+        throw new Error('missing endpoint property');
+      }
+    }
+
+    babelHelpers.createClass(HalApi, [{
+      key: 'request',
+      value: function request(fn) {
+        if (fn) {
+          Resource.request = fn;
+        }
+      }
+    }, {
+      key: 'resource',
+      value: function resource(path) {
+        return Resource.fetch(path, this.options);
+      }
+    }]);
+    return HalApi;
+  })();
+
+  return HalApi;
+
+}));
