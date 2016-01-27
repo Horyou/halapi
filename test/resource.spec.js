f03dcf50a028804ae3816ad661d030a0ff5bd151
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-27 21:30:05 +0100

    test(resource): migrate to async test
    This enable to fix some problems with test non being complete
    even while using a promise and resolving it or calling test.end()

--- test/resource.spec.js
+++ test/resource.spec.js
@@ -1,295 +1,312 @@
 'use strict';
 import Resource from '../source/resource';
-import test from 'tape';
+import asyncTest from './async-test';
+
 import has from 'lodash/object/has';
 import { isUndefined, isEmpty } from 'lodash/lang';
-import { server, apiServer } from './helpers';
 
-test('Resource fetch', (t) => {
-  server().then((api) => {
-    const result = Resource.fetch('/api', api.options);
 
-    t.equal(typeof result.then, 'function', 'should be a promise');
+asyncTest('Resource fetch', (t, before, done) => {
+  before();
+  t.comment('test');
+  const result = Resource.fetch('/api', { endpoint: 'foo' });
 
-    result.then((resource) => {
+  t.equal(typeof result.then, 'function', 'should be a promise');
+
+  result.then(resource => {
+    t.ok(resource instanceof Resource, 'should return a Resource instance');
+
+    done();
+  });
+});
+
+asyncTest('Resource data', (t, before, done) => {
+  before({ body: 'ok' });
+
+  Resource.fetch('/api', { endpoint: 'foo' })
+    .then((resource) => {
       t.ok(resource instanceof Resource, 'should return a Resource instance');
+      t.equal(resource._data, 'ok', 'resource data should hold the response body');
 
-      t.end();
+      done();
     });
-  });
 });
 
-test('Resource data', (t) => {
-  server().then((api) => {
-    Resource
-      .fetch('/api', api.options)
-      .then((resource) => {
-        t.ok(resource instanceof Resource, 'should return a Resource instance');
-        t.equal(resource._data, 'ok', 'resource data should hold the response body');
+asyncTest('Resource links with default `links` attributes', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end()
-      });
-  });
+  Resource
+    .fetch('/person/1', { endpoint: 'foo' })
+    .then(resource => {
+      const links = resource.links();
+
+      t.ok(has(links, 'self'), 'should have the `self` link');
+      t.ok(has(links, 'house'), 'should have the `house` link');
+
+      done();
+    });
 });
 
-test('Resource links with default `links` attributes', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then(resource => {
-        const links = resource.links();
+asyncTest('Resource links with `_links` attributes', (t, before, done) => {
+  before({ body: require('./fixtures/response/person-_links.json') });
 
-        t.ok(has(links, 'self'), 'should have the `self` link');
-        t.ok(has(links, 'house'), 'should have the `house` link');
+  const options = {
+    endpoint: 'foo',
+    linkAttr: '_links'
+  };
 
-        t.end();
-      });
-  });
+  Resource
+    .fetch('/person/1', options)
+    .then((resource) => {
+      const links = resource.links();
+
+      t.ok(has(links, 'self'), 'should have the `self` link');
+      t.ok(has(links, 'house'), 'should have the `house` link');
+
+      done();
+    });
 });
 
-test('Resource links with `_links` attributes', (t) => {
-  apiServer('resource-_links.json').then((api) => {
-    api.options.linkAttr = '_links';
+asyncTest('Resource unexisting link', (t, before, done) => {
+  before({ body: require('./fixtures/response/person-nolinks.json') });
 
-    Resource
-      .fetch('/person/1', api.options)
-      .then((resource) => {
-        const links = resource.links();
+  Resource
+    .fetch('/person/1', { endpoint: 'foo' })
+    .then((resource) => {
+      t.equal(resource.link('home'), null, 'undefined link should be null');
 
-        t.ok(has(links, 'self'), 'should have the `self` link');
-        t.ok(has(links, 'house'), 'should have the `house` link');
-
-        t.end();
-      });
-  });
+      done();
+    });
 });
 
-test('Resource unexisting link', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((resource) => {
-        t.equal(resource.link('home'), null, 'undefined link should be null');
+asyncTest('Resource hasLink', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end();
-      });
-  });
+  const resource = new Resource('/person/1', { endpoint: 'foo' });
+
+  resource.fetch()
+    .then((person) => {
+      t.ok(person.hasLink('house'), 'should tell `house` link exists');
+      t.notOk(person.hasLink('houze'), 'should tell `houze` link does not exist');
+
+      done();
+    });
 });
 
-test('Resource link', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    const resource = new Resource('/person/1', api.options);
 
-    resource.fetch()
-      .then((person) => {
-        const link = person.link('house');
+asyncTest('Resource link', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.ok(link instanceof Resource, 'link should return a Resource');
-        t.equal(link.path(), '/person/1/house', 'should return a promise');
+  const resource = new Resource('/person/1', { endpoint: 'foo' });
 
-        t.end();
-      });
-  });
+  resource.fetch()
+    .then((person) => {
+      const link = person.link('house');
+
+      t.ok(link instanceof Resource, 'link should return a Resource');
+      t.equal(link.path(), '/person/1/house', 'should return a promise');
+
+      done();
+    });
 });
 
-test('Resource bad link', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        const link = person.link('badLink');
+asyncTest('Resource bad link', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.equal(link, null, 'should not return a promise for a link without href');
+  Resource
+    .fetch('/person/1', { endpoint: 'foo' })
+    .then((person) => {
+      const link = person.link('badLink');
 
-        t.end();
-      });
-  });
+      t.equal(link, null, 'should not return a promise for a link without href');
+
+      done();
+    });
 });
 
-test('Resource without links', (t) => {
-  apiServer('resource-nolinks.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        const link = person.link('foo');
+asyncTest('Resource without links', (t, before, done) => {
+  before({ body: require('./fixtures/response/person-nolinks.json') });
 
-        t.equal(link, null, 'should not return a promise if no links');
+  Resource
+    .fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      const link = person.link('foo');
 
-        t.end();
-      });
-  });
+      t.equal(link, null, 'should not return a promise if no links');
+
+      done();
+    });
 });
 
-test('Resource linked resource', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    const resource = new Resource('/person/1', api.options);
+asyncTest('Resource linked resource', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-    resource.fetch()
-      .then((person) => {
-        const link = person.link('house');
+  const resource = new Resource('/person/1', { endpoint: '/foo' });
 
-        return link.fetch();
-      })
-      .then((house) => {
-        t.ok(house instanceof Resource, 'should resolve a resource');
-        t.equal(house.path(), '/person/1/house', 'should have the correct path');
+  resource.fetch()
+    .then((person) => {
+      const link = person.link('house');
 
-        t.equal(house.data().name, 'The little house', 'should have the correct data');
+      before({ body: require('./fixtures/response/house.json') });
 
-        t.end();
-      });
-  });
+      return link.fetch();
+    })
+    .then((house) => {
+      t.ok(house instanceof Resource, 'should resolve a resource');
+      t.equal(house.path(), '/person/1/house', 'should have the correct path');
+
+      t.equal(house.data().name, 'The little house', 'should have the correct data');
+
+      done();
+    });
 });
 
-test('Resource: resource helper with a valid link', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource.fetch('/person/1', api.options)
-      .then((person) => {
-        const link = person.resource('house');
+asyncTest('Resource: resource helper with a valid link', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.equal(typeof link.then, 'function', 'should return a promise');
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      before({ body: require('./fixtures/response/house.json') });
 
-        return link;
-      })
-      .then(value => {
-        t.ok(value instanceof Resource, 'link should return a Resource');
-        t.equal(value.path(), '/person/1/house', 'should have the correct path');
-        t.equal(value.data().name, 'The little house', 'should have been fetched');
+      const link = person.resource('house');
 
-        t.end();
-      });
-  });
+      t.equal(typeof link.then, 'function', 'should return a promise');
+
+      return link;
+    })
+    .then(value => {
+      t.ok(value instanceof Resource, 'link should return a Resource');
+      t.equal(value.path(), '/person/1/house', 'should have the correct path');
+      t.equal(value.data().name, 'The little house', 'should have been fetched');
+
+      done();
+    });
 });
 
-test('Resource: resource helper with an invalid link', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource.fetch('/person/1', api.options)
-      .then((person) => {
-        const link = person.resource('houze');
+asyncTest('Resource: resource helper with an invalid link', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.equal(typeof link.then, 'function', 'should return a promise');
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      const link = person.resource('houze');
 
-        return link;
-      })
-      .catch(err => {
-        t.equal(err.message, 'The resource <houze> does not exists', 'should reject the promise');
+      t.equal(typeof link.then, 'function', 'should return a promise');
 
-        t.end();
-      });
-  });
+      return link;
+    })
+    .catch(err => {
+      t.equal(err.message, 'The resource <houze> does not exists', 'should reject the promise');
+
+      done();
+    });
 });
 
-test('Resource get attribute', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        t.equal(person.get('firstname'), 'John', 'should return field value');
+asyncTest('Resource get attribute', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end();
-      });
-  });
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      t.equal(person.get('firstname'), 'John', 'should return field value');
+
+      done();
+    });
 });
 
-test('Resource get wrong attribute', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        t.ok(isUndefined(person.get('firsname')), 'should return undefined');
+asyncTest('Resource get wrong attribute', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end();
-      });
-  });
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      t.ok(isUndefined(person.get('firsname')), 'should return undefined');
+
+      done();
+    });
 });
 
-test('Resource: get attribute of a linked resource', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        return person.link('house').fetch();
-      })
-      .then((house) => {
-        t.equal(house.get('name'), 'The little house', 'should return the linked field value');
+asyncTest('Resource: get attribute of a linked resource', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end();
-      });
-  });
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      before({ body: require('./fixtures/response/house.json') });
+
+      return person.link('house').fetch();
+    })
+    .then((house) => {
+      t.equal(house.get('name'), 'The little house', 'should return the linked field value');
+
+      done();
+    });
 });
 
-test('Resource: data fields', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        const data = person.data();
+asyncTest('Resource: data fields', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.ok(!has(data, api.options.linkAttr), 'should not export links property');
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      const data = person.data();
 
-        t.end();
-      });
-  });
+      t.ok(!has(data, 'links'), 'should not export links property');
+
+      done();
+    });
 });
 
-test('Resource: response data', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((resource) => {
-        t.equal(resource.json(), resource._data, 'fetched data should be exported using json()');
+asyncTest('Resource: response data', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end();
-      });
-  });
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((resource) => {
+      t.equal(resource.json(), resource._data, 'fetched data should be exported using json()');
+
+      done();
+    });
 });
 
-test('Resource: request path', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((person) => {
-        t.equal(person.path(), '/person/1', 'should export request path');
+asyncTest('Resource: request path', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.end();
-      });
-  });
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((person) => {
+      t.equal(person.path(), '/person/1', 'should export request path');
+
+      done();
+    });
 });
 
-test('Resource: url', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    Resource
-      .fetch('/person/1', api.options)
-      .then((resource) => {
-        const endpoint = resource.options.endpoint;
+asyncTest('Resource: url', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-        t.equal(resource.url(), endpoint + '/person/1', 'should return the absolute url using url()');
+  Resource.fetch('/person/1', { endpoint: '/foo' })
+    .then((resource) => {
+      const endpoint = resource.options.endpoint;
 
-        t.end();
-      });
-  });
+      t.equal(resource.url(), endpoint + '/person/1', 'should return the absolute url using url()');
+
+      done();
+    });
 })
 
-test('Resource: fetch', (t) => {
-  apiServer('resource-links.json').then((api) => {
-    const resource = new Resource('/person/1', api.options);
+asyncTest('Resource: fetch', (t, before, done) => {
+  before({ body: require('./fixtures/response/person.json') });
 
-    t.ok(isEmpty(resource.data()), 'body should not be set before fetch');
+  const resource = new Resource('/person/1', { endpoint: '/foo' })
 
-    resource.fetch().then((res) => {
-      t.ok(res instanceof Resource, 'should return a Resource');
-      t.equal(resource, res, 'should return the resource');
+  t.ok(isEmpty(resource.data()), 'body should not be set before fetch');
 
-      const data = resource.data();
+  resource.fetch().then((res) => {
+    t.ok(res instanceof Resource, 'should return a Resource');
+    t.equal(resource, res, 'should return the resource');
 
-      t.equal(data.firstname, 'John', 'body should be set after fetch');
+    const data = resource.data();
 
-      t.end();
-    });
+    t.equal(data.firstname, 'John', 'body should be set after fetch');
+
+    done();
   });
 });
 
-test('Resource: save', (t) => {
+asyncTest('Resource: save', (t, before, done) => {
   const resource = new Resource('/foo');
 
   t.ok(isEmpty(resource.data()), 'body should not be set before fetch');
@@ -300,5 +317,5 @@ test('Resource: save', (t) => {
 
   t.deepEqual(resource.data(), { foo: 'bar' }, 'should save the data');
 
-  t.end();
+  done();
 });
